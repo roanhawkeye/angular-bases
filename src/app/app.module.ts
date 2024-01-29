4ac7eea72a0b56f9ff75483eec50700f194c9644
Daniel Martinez <roanhawkeye@gmail.com>
2024-01-29 13:24:55 -0600

    Finish base app course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { CounterModule } from './counter/counter.module'
 import { HeroesModule } from './heroes/heroes.module';
 ;
 import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
